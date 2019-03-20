698ab7a522837c774075f05a87517e1cd6f19ff8
samishegga <47670429+samishegga@users.noreply.github.com>
2019-03-20 15:19:30 +0300

    ToastmakeText(Myactivity.this, "Hello" +name.getText().toString() Concatenation of hello message with user input from variable of type EditText(name).

--- app/src/main/java/com/example/myapp1/MainActivity.java
+++ app/src/main/java/com/example/myapp1/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.myapp1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void logIn(View view) {
+        EditText name=(EditText)findViewById(R.id.username);
+        Button button=(Button)findViewById(R.id.buttonClick);
+        Toast.makeText(MainActivity.this,"Hello "+name.getText().toString() ,
+                Toast.LENGTH_LONG).show();
+
     }
 }
